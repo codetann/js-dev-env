49c40f766e0fd6d23f3c8bfb35a00f1466391385
Tanner <thetannerthomas@gmail.com>
2021-07-21 11:41:09 -0600

    changed folder name

---
+++ build-scripts/srcServer.js
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+import open from "open";
+
+const port = 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../src/index.html"));
+});
+
+app.listen(port, (err) =>
+  err ? console.log(err) : open("http://localhost:" + port)
+);
