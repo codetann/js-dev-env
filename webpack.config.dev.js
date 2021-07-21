a61d779a879478361cb91adce06978ff61b5efc6
Tanner <thetannerthomas@gmail.com>
2021-07-21 13:40:16 -0600

    fixed typo error

---
+++ webpack.config.dev.js
@@ -0,0 +1,23 @@
+import path from "path";
+
+export default {
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: "./src/index.js",
+  // development mode wont acutally create files, saves in memory and serves them
+  output: {
+    path: path.resolve(__dirname, "src"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  plugins: [],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
+      {
+        test: /\.sass$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
