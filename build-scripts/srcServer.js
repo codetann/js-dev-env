14fd2d11ab111ce720912f20e0948bc48f44893b
Tanner <thetannerthomas@gmail.com>
2021-07-21 14:26:38 -0600

    created users route

--- build-scripts/srcServer.js
+++ build-scripts/srcServer.js
@@ -8,6 +8,13 @@ const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
+// Hard coded data. Pretend this hits a real database
+const testDB = [
+  { id: 1, firstName: "Bob", lastName: "Smith", email: "bob@gmail.com" },
+  { id: 1, firstName: "Tammy", lastName: "Norton", email: "tamm@gmail.com" },
+  { id: 1, firstName: "Tina", lastName: "Lee", email: "tina@gmail.com" },
+];
+
 app.use(
   require("webpack-dev-middleware")(compiler, {
     publicPath: config.output.publicPath,
@@ -16,6 +23,10 @@ app.use(
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../src/index.html"));
+});
+
+app.get("/users", (req, res) => {
+  res.json(testDB);
 });
 
 app.listen(port, (err) =>
