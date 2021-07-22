8ee7dc2eeb35172f64f6c748194d6d7270550cb4
Tanner <thetannerthomas@gmail.com>
2021-07-22 09:39:47 -0600

    initial commit

---
+++ webpack.config.prod.js
@@ -0,0 +1,28 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  mode: "production",
+  devtool: "source-map",
+  entry: "./src/index.js",
+  // development mode wont acutally create files, saves in memory and serves them
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
+      {
+        test: /\.sass$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
