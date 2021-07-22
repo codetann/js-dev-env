374694cb554441087b05ef53887727277cd40627
Tanner <thetannerthomas@gmail.com>
2021-07-22 09:58:07 -0600

    added minicss

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export default {
   mode: "production",
@@ -8,13 +9,15 @@ export default {
     main: path.resolve(__dirname, "src/index"),
     vendor: path.resolve(__dirname, "src/vendor"),
   },
-  // development mode wont acutally create files, saves in memory and serves them
   output: {
     path: path.resolve(__dirname, "dist"),
     publicPath: "/",
-    filename: "[name].js",
+    filename: "[name].[chunkhash].js",
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash].css",
+    }),
     new HtmlWebpackPlugin({
       template: "src/index.html",
     }),
@@ -24,7 +27,7 @@ export default {
       { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
       {
         test: /\.sass$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
