7231a4cac7307eb27369c48b968ddb24139a0193
Tanner <thetannerthomas@gmail.com>
2021-07-22 09:41:54 -0600

    updated mock api

---
+++ src/api/baseUrl.js
@@ -0,0 +1,5 @@
+export default function getBaseUrl() {
+  return getQueryStringParameterByName("useMockApi")
+    ? "http://localhost:3001"
+    : "/";
+}
--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,12 +1,27 @@
 import "whatwg-fetch";
+import getBaseUrl from "./baseUrl";
+
+const baseUrl = getBaseUrl();
 
 export function getUsers() {
   return get("users");
 }
 
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+
 // Fetch data based an url entered
 function get(url) {
-  return fetch(url).then(onSuccess, onError);
+  return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+function del(url) {
+  const req = new Request(baseUrl + url, {
+    method: "DELETE",
+  });
+
+  return fetch(req).then(onSuccess, onError);
 }
 
 // Runs on get(url) success
